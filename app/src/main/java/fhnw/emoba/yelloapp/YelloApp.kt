8498c06ee605aaebebd669e156148ff6f8044acd
Florian Daasch <47759697+Florian270496@users.noreply.github.com>
2021-01-09 19:38:21 +0100

    Deleted comments in UI

--- app/src/main/java/fhnw/emoba/yelloapp/YelloApp.kt
+++ app/src/main/java/fhnw/emoba/yelloapp/YelloApp.kt
@@ -15,7 +15,7 @@ object YelloApp : EmobaApp {
     override fun initialize(activity: AppCompatActivity, savedInstanceState: Bundle?) {
 
         /// For simulator:
-        val connector = TelloConnector(ip          = "192.168.1.6",
+        val connector = TelloConnector(ip          = "192.168.0.18",
             commandPort = 8879,
             statePort   = 8890)
 
